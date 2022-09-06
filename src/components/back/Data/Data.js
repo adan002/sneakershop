65ff07836f33a7f0ef35017007a5364731661932
adan villalpando <adan_1990.icloud.com>
2022-09-06 10:03:45 -0600

    first commit

---
+++ src/components/back/Data/Data.js
@@ -0,0 +1,62 @@
+const data = {
+  productItems: [
+    {
+      id: "1",
+      name: "Air Jordan 1 Retro High OG University Blue",
+      price: 110.0,
+      image:
+        "https://houseofheat.co/app/uploads/2021/01/air-jordan-1-high-og-university-blue-2021-555088-134-release-date-1.jpg",
+    },
+    {
+      id: "2",
+      name: "Air Jordan 1 Retro High OG Chicago Red",
+      price: 110.0,
+      image:
+        "https://www.freshnessmag.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTM1OTgwOTQ5MjQ3ODU3OTM4/air-jordan-1-retro-high-og-chicago-release-00.jpg",
+    },
+    {
+      id: "3",
+      name: "Air Jordan 1 Mid Light Smoke Grey",
+      price: 110.0,
+      image:
+        "https://media.endclothing.com/end-features/f_auto,q_auto:eco,w_1520/prodfeatures/f597bd45-bc3e-4498-b9fb-5e281558090f_Air+Jordan+1+Mid+PS1.png?auto=compress,format",
+    },
+    {
+      id: "4",
+      name: "Off-White x Air Jordan 1 Retro High OG UNC",
+      price: 110.0,
+      image:
+        "https://sneakers-magazine.com/wp-content/uploads/2018/05/off-white-x-air-jordan-1-unc-aq0818-148-facebook.jpg",
+    },
+    {
+      id: "5",
+      name: "Air Jordan 1 Retro High OG Black/White",
+      price: 110.0,
+      image:
+        "https://www.sneakerfiles.com/wp-content/uploads/2019/04/air-jordan-1-panda-wmns-CD0461-007-release-date-price.jpg",
+    },
+    {
+      id: "6",
+      name: "Air Jordan 1 Retro High OG Premium Yin Yang",
+      price: 110.0,
+      image:
+        "https://www.kicksonfire.com/wp-content/uploads/2016/07/Air-Jordan-1-High-OG-Ying-Yang-Pack-White-1.jpg",
+    },
+    {
+      id: "7",
+      name: "Air Jordan 1 Retro High OG Game Royal",
+      price: 110.0,
+      image:
+        "https://media.endclothing.com/media/catalog/product/2/3/23-03-2018_nike_airjordan1retrohighog_gameroyalblack_white_555088-403_hh_1.jpg",
+    },
+    {
+      id: "8",
+      name: "Air Jordan 1 Retro High OG Pine Green",
+      price: 110.0,
+      image:
+        "https://sneakerbardetroit.com/wp-content/uploads/2018/09/Air-Jordan-1-Pine-Green-555088-302-Release-Date-Price-4.jpg",
+    },
+  ],
+};
+
+export default data;
---
+++ src/components/front/Signup/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Signup.css";
+// import Header from "../Header/Header";
+
+const Signup = () => {
+    // <div>
+    // <Header />
+    // </div>
+  return <div className="signup">Signup</div>;
+};
+
+export default Signup;
